7911dba597db83ffcfb16403d93c7ac55c0e7663
mekb <mekbgithub@icloud.com>
2023-12-28 09:38:07 +1100

    :sparkles: Make shader/CUDA files treated as C, add more formatters

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -60,3 +60,8 @@ g.neovide_cursor_vfx_mode = "railgun"
 --		augroup END
 --	]])
 --end
+
+vim.cmd("autocmd BufRead,BufNewFile *.fsh set filetype=c")
+vim.cmd("autocmd BufRead,BufNewFile *.vsh set filetype=c")
+vim.cmd("autocmd BufRead,BufNewFile *.glsl set filetype=c")
+vim.cmd("autocmd BufRead,BufNewFile *.cu set filetype=c")
--- lua/plugins/configs/formatter.lua
+++ lua/plugins/configs/formatter.lua
@@ -7,6 +7,8 @@ return function()
 			c = { require("formatter.filetypes.c").clangformat },
 			cpp = { require("formatter.filetypes.cpp").clangformat },
 			json = { require("formatter.filetypes.json").jq },
+			sh = { require("formatter.filetypes.sh").shfmt },
+			javascript = { require("formatter.filetypes.javascript").eslint_d },
 			["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 		},
 	})
