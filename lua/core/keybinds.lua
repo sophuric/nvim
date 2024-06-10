04b59eaa3ea68f669af7cac589c2255d2ad1d635
mekb <mekbgithub@icloud.com>
2024-06-10 18:52:22 +1000

    :sparkles: Tweak keybinds

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -1,6 +1,13 @@
 -- Keybinds
 vim.g.mapleader = " "
 
+local function cmd(name, func)
+	local id = name:gsub("(%w)(%w*)", function(first, rest)
+		return first:upper() .. rest:lower() -- title case
+	end):gsub("[^0-9A-Za-z]", "")
+	vim.api.nvim_create_user_command(id, func, {})
+	return id
+end
 local function map(mode, keystroke, out, desc)
 	if mode == "all" then
 		mode = { "n", "v", "x", "s", "o", "i", "l", "c", "t" }
@@ -10,6 +17,10 @@ local function map(mode, keystroke, out, desc)
 	end
 	if type(keystroke) ~= "table" then
 		keystroke = { keystroke }
+	end
+	if type(out) == "function" then
+		local id = cmd(desc, out)
+		out = "<cmd>" .. id .. "<CR>"
 	end
 	for _, m in ipairs(mode) do
 		for _, k in ipairs(keystroke) do
@@ -21,69 +32,63 @@ local function map(mode, keystroke, out, desc)
 		end
 	end
 end
-local function cmd(name, func)
-	vim.api.nvim_create_user_command(name, func, {})
-end
 
--- Tab keybinds, some are overriden by my kitty config, but not in neovide
+-- Reminiscent of kitty keybinds, some are overriden from my kitty config, but they work in neovide and other terminals
+
 map("n", "<C-`>", "<cmd>BufferLineCycleNext<CR>", "Next tab")
-map("n", "<C-S-`>", "<cmd>BufferLineCyclePrev<CR>", "Previous tab")
 map("n", "<C-Tab>", "<cmd>BufferLineCycleNext<CR>", "Next tab")
+map("n", "<C-Right>", "<cmd>BufferLineCycleNext<CR>", "Next tab")
+map("n", "<C-S-Right>", "<cmd>BufferLineCycleNext<CR>", "Next tab")
+
+map("n", "<C-S-`>", "<cmd>BufferLineCyclePrev<CR>", "Previous tab")
 map("n", "<C-S-Tab>", "<cmd>BufferLineCyclePrev<CR>", "Previous tab")
-map("n", "<C-.>", "<cmd>BufferLineMoveNext<CR>", "Move tab right")
-map("n", "<C-S-.>", "<cmd>BufferLineMoveNext<CR>", "Move tab right")
-map("n", "<C-,>", "<cmd>BufferLineMovePrev<CR>", "Move tab left")
-map("n", "<C-S-,>", "<cmd>BufferLineMovePrev<CR>", "Move tab left")
-map("n", "<C-Right>", "<cmd>BufferLineCycleNext<CR>", "Next Tab")
-map("n", "<C-S-Right>", "<cmd>BufferLineCycleNext<CR>", "Next Tab")
 map("n", "<C-Left>", "<cmd>BufferLineCyclePrev<CR>", "Previous tab")
 map("n", "<C-S-Left>", "<cmd>BufferLineCyclePrev<CR>", "Previous tab")
+
+map("n", "<C-.>", "<cmd>BufferLineMoveNext<CR>", "Move tab right")
+map("n", "<C-S-.>", "<cmd>BufferLineMoveNext<CR>", "Move tab right")
+
+map("n", "<C-,>", "<cmd>BufferLineMovePrev<CR>", "Move tab left")
+map("n", "<C-S-,>", "<cmd>BufferLineMovePrev<CR>", "Move tab left")
+
+-- ^W switches windows in vim, so ^Q is used instead
 map("n", "<C-Q>", "<cmd>bd<CR>", "Close buffer")
 map("n", "<C-S-Q>", "<cmd>bd<CR>", "Close buffer")
 map("n", "<C-T>", "<cmd>enew<CR>", "New buffer")
 map("n", "<C-S-T>", "<cmd>enew<CR>", "New buffer")
 
--- Paste and copy
-map("n", "<C-S-C>", "yy", "Copy")
-map("v", "<C-S-C>", "y", "Copy")
-map("n", "<C-S-V>", "vpi", "Paste")
-map("i", "<C-S-V>", "<ESC>vpi", "Paste")
+local rename = function()
+	require("renamer").rename()
+end
 
 -- Rename
-map("i", "<C-K><C-R>", "<cmd>lua require(\"renamer\").rename()<CR>", "Paste")
-map("n", "<C-K><C-R>", "<cmd>lua require(\"renamer\").rename()<CR>", "Paste")
-map("v", "<C-K><C-R>", "<cmd>lua require(\"renamer\").rename()<CR>", "Paste")
+map("i", "<C-K><C-R>", rename, "Rename")
+map("n", "<C-K><C-R>", rename, "Rename")
+map("v", "<C-K><C-R>", rename, "Rename")
 
 -- Disable middle mouse button paste
 map("all", { "<MiddleMouse>", "<2-MiddleMouse>", "<3-MiddleMouse>", "<4-MiddleMouse>" }, "")
 
--- Functions
-cmd("FindProjects", function()
-	require("telescope").extensions.projects.projects({})
-end)
-cmd("FileHistory", function()
-	require("telescope.builtin").oldfiles()
-end)
-cmd("PromptGoLine", function()
+-- Other keybinds
+map("n", "<C-O>", "<cmd>NvimTreeToggle<CR>", "Toggle tree view")
+map("n", "<C-S-O>", "<cmd>NvimTreeFindFile<CR>", "Focus current file in tree view")
+map("n", "<C-D>", "<cmd>ToggleTerm direction=horizontal<CR>", "Toggle terminal")
+map("n", "<C-G>", function()
 	local line = vim.fn.input("Go to line: ")
 	if tonumber(line, 10) ~= nil then
 		vim.cmd("<cmd>" .. line)
 	else
 		print("Not a number")
 	end
-end)
+end, "Go to line")
 
--- Other keybinds
-map("n", "<C-O>", "<cmd>NvimTreeToggle<CR>", "Toggle tree view")
-map("n", "<C-S-O>", "<cmd>NvimTreeFindFile<CR>", "Focus current file in tree view")
-map("n", "<C-D>", "<cmd>ToggleTerm direction=horizontal<CR>", "Toggle terminal")
-map("n", "<C-G>", "<cmd>PromptGoLine<CR>", "Go to line")
-
-cmd("Reload", function()
-	vim.cmd("<cmd>source $MYVIMRC")
-end)
-map("n", "fr", "<cmd>FindProjects<CR>", "Find projects")
-map("n", "fh", "<cmd>FileHistory<CR>", "Search File History")
+-- Telescope keybinds
+map("n", "fr", function()
+	require("telescope").extensions.projects.projects({})
+end, "Find projects")
+map("n", "fh", function()
+	require("telescope.builtin").oldfiles()
+end, "File history")
 
 map("n", "<C-/>", '<cmd>let @/ = ""<CR>', "Clear search query")
 map("n", "<C-Up>", "g<Up>", "Go up visual line")
