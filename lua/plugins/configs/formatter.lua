de617446f8e361863306ed71175828546663b878
sophur <mekbgithub@icloud.com>
2025-08-02 18:34:25 +1000

    🐛 Fix broken JS formatter

--- lua/plugins/configs/formatter.lua
+++ lua/plugins/configs/formatter.lua
@@ -1,37 +1,26 @@
 return function()
-	local js = {
-		function()
-			return {
-				exe = "eslint",
-				args = {
-					"--fix",
-				},
-				stdin = false,
-				try_node_modules = true,
-			}
-		end,
-	}
+	local filetypes = "formatter.filetypes."
 	require("formatter").setup({
 		logging = true,
 		log_level = vim.log.levels.WARN,
 		-- full list at https://github.com/mhartington/formatter.nvim/tree/master/lua/formatter/filetypes
 		filetype = {
-			lua = { require("formatter.filetypes.lua").stylua },
-			c = { require("formatter.filetypes.c").clangformat },
-			cpp = { require("formatter.filetypes.cpp").clangformat },
-			cs = { require("formatter.filetypes.cs").dotnetformat },
-			json = { require("formatter.filetypes.json").jq },
-			sh = { require("formatter.filetypes.sh").shfmt },
-			rust = { require("formatter.filetypes.rust").rustfmt },
-			typescript = js,
-			javascript = js,
-			html = { require("formatter.filetypes.html").tidy },
-			css = { require("formatter.filetypes.css").cssbeautify },
-			yaml = { require("formatter.filetypes.yaml").yamlfmt },
-			xml = { require("formatter.filetypes.xml").xmllint },
-			perl = { require("formatter.filetypes.perl").perltidy },
-			python = { require("formatter.filetypes.python").black },
-			["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
+			lua = { require(filetypes .. "lua").stylua },
+			c = { require(filetypes .. "c").clangformat },
+			cpp = { require(filetypes .. "cpp").clangformat },
+			cs = { require(filetypes .. "cs").dotnetformat },
+			json = { require(filetypes .. "json").jq },
+			sh = { require(filetypes .. "sh").shfmt },
+			rust = { require(filetypes .. "rust").rustfmt },
+			typescript = require(filetypes .. "javascript").eslint_d,
+			javascript = require(filetypes .. "javascript").eslint_d,
+			html = { require(filetypes .. "html").tidy },
+			css = { require(filetypes .. "css").cssbeautify },
+			yaml = { require(filetypes .. "yaml").yamlfmt },
+			xml = { require(filetypes .. "xml").xmllint },
+			perl = { require(filetypes .. "perl").perltidy },
+			python = { require(filetypes .. "python").black },
+			["*"] = { require(filetypes .. "any").remove_trailing_whitespace },
 		},
 	})
 end
